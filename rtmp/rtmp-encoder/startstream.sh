fa327ff4fe9e40b2fd5c70af07b8647cc6f4725a
Mikael Nervik Eidsvaag <58994869+git-vcs@users.noreply.github.com>
2021-06-26 21:41:59 +0200

    make compose file for automatic star of both rtmp server and encoder
     Changes to be committed:
    	new file:   rtmp/docker-compose.yml
    	renamed:    rtmp-encoder/Dockerfile -> rtmp/rtmp-encoder/Dockerfile
    	new file:   rtmp/rtmp-encoder/docker-compose.yml
    	renamed:    rtmp-encoder/startstream.sh -> rtmp/rtmp-encoder/startstream.sh
    	renamed:    rtmp-server/Dockerfile -> rtmp/rtmp-server/Dockerfile.ubuntu
    	renamed:    rtmp-server/docker-compose.yml -> rtmp/rtmp-server/docker-compose.yml
    	renamed:    rtmp-server/nginx.conf -> rtmp/rtmp-server/nginx.conf
    	renamed:    rtmp-server/startserver.sh -> rtmp/rtmp-server/startserver.sh

--- rtmp/rtmp-encoder/startstream.sh
+++ rtmp/rtmp-encoder/startstream.sh
@@ -8,9 +8,9 @@ while true
 			sleep 5
 			ffmpeg -re -f v4l2 -i `ls  /dev/video*` -vcodec libx264 -preset ultrafast -f flv rtmp://$url
 		else
-			echo using defuelt rtmp server
+			echo using default rtmp server
 			sleep 5
-			ffmpeg -re -f v4l2 -i `ls  /dev/video*` -vcodec libx264 -preset ultrafast -f flv rtmp://10.0.1.55:1935/live/b
+			ffmpeg -re -f v4l2 -i `ls  /dev/video*` -vcodec libx264 -preset ultrafast -f flv rtmp://127.0.0.1:1935/live/b
 		fi
 		sleep 2
 	done
