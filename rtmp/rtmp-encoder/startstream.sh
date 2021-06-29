ee81f7052d38dc8d9d50be806a34ee2ab5602c37
Mikael Nervik Eidsvaag <58994869+git-vcs@users.noreply.github.com>
2021-06-29 12:32:47 +0200

    added ffmpeg trhreads limmit 	modified:   rtmp/docker-compose.yml 	modified:   rtmp/rtmp-encoder/docker-compose.yml 	modified:   rtmp/rtmp-encoder/startstream.sh

--- rtmp/rtmp-encoder/startstream.sh
+++ rtmp/rtmp-encoder/startstream.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-
 while true
 	do
 		if test "$url"
 		then
 			echo using supplied rtmp server $url
 			sleep 5
-			ffmpeg -re -f v4l2 -i `ls  /dev/video*` -vcodec libx264 -preset ultrafast -f flv rtmp://$url
+			ffmpeg -re -f v4l2 -i `ls  /dev/video*` -vcodec libx264 -preset ultrafast -threads $FFMPEG_THREADS -f flv rtmp://$url
 		else
 			echo using default rtmp server
 			sleep 5
-			ffmpeg -re -f v4l2 -i `ls  /dev/video*` -vcodec libx264 -preset ultrafast -f flv rtmp://127.0.0.1:1935/live/b
+			ffmpeg -re -f v4l2 -i `ls  /dev/video*` -vcodec libx264 -preset ultrafast -threads $FFMPEG_THREADS -f flv rtmp://127.0.0.1:1935/live/b
 		fi
 		sleep 2
 	done
