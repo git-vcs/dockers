c09da6239c2e4f142b51da9a5300042f67ea46d5
Mikael Nervik Eidsvaag <58994869+git-vcs@users.noreply.github.com>
2021-07-01 18:43:35 +0200

    started adding audio fuctions

--- rtmp/rtmp-encoder/startstream.sh
+++ rtmp/rtmp-encoder/startstream.sh
@@ -5,11 +5,11 @@ while true
 		then
 			echo using supplied rtmp server $url
 			sleep 5
-			ffmpeg -re -f v4l2 -i `ls  /dev/video*` -vcodec libx264 -preset ultrafast -threads $FFMPEG_THREADS -f flv rtmp://$url
+			ffmpeg -re -f v4l2 -i /dev/video0 -vcodec libx264 -preset ultrafast -threads $FFMPEG_THREADS -f flv rtmp://$url
 		else
 			echo using default rtmp server
 			sleep 5
-			ffmpeg -re -f v4l2 -i `ls  /dev/video*` -vcodec libx264 -preset ultrafast -threads $FFMPEG_THREADS -f flv rtmp://127.0.0.1:1935/live/b
+			ffmpeg -re -f v4l2 -i /dev/video0 -vcodec libx264 -preset ultrafast -threads $FFMPEG_THREADS -f flv rtmp://127.0.0.1:1935/live/b
 		fi
 		sleep 2
 	done
